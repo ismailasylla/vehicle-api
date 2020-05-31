4d13ba569c3eadd32bb58a519cc005f31ecc599f
ismailasylla <ismailasyllaa@gmail.com>
2020-06-01 02:57:10 +0400

    Added mongoose/ implemented the schema/model and tested the operations requests to the endpoints

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const vehicles = require('./routes/vehicles');
 const home = require('./routes/home');
+const mongoose = require('mongoose');
 
 const app = express();
+// connection to the database
+mongoose
+  .connect('mongodb://localhost/vehicles', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('connected'))
+  .catch((err) => console.log('Could not connect to the database!', err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,93 +1,99 @@
 const express = require('express');
 const router = express.Router();
 const Joi = require('@hapi/joi');
+const mongoose = require('mongoose');
+
+const Vehicle = mongoose.model(
+  'Vehicle',
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlenght: 15,
+    },
+  })
+);
 
 // Vehicles Array
-const vehicles = [];
+// const vehicles = [];
 
 // Get all vehicles
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const vehicles = await Vehicle.find().sort('name');
   res.send(vehicles);
 });
 
 // Create Vehicle
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { error } = validateVehicle(req.body);
 
   if (error) return res.status(400).send(error.details[0].message);
 
-  const vehicle = {
-    id: vehicles.length + 1,
+  let vehicle = new Vehicle({
     name: req.body.name,
-    category: req.body.category,
-    transmission: req.body.transmission,
-    engine: req.body.engine,
-    fuelType: req.body.fuelType,
-    colorExterior: req.body.colorExterior,
-    colorInterior: req.body.colorInterior,
-    price: req.body.price,
-  };
-  vehicles.push(vehicle);
+    // category: req.body.category,
+    // transmission: req.body.transmission,
+    // engine: req.body.engine,
+    // fuelType: req.body.fuelType,
+    // colorExterior: req.body.colorExterior,
+    // colorInterior: req.body.colorInterior,
+    // price: req.body.price,
+  });
+  vehicle = await vehicle.save();
   res.send(vehicle);
 });
 
-// Get single vehicle
-router.get('/:id', (req, res) => {
-  const vehicle = vehicles.find((v) => v.id === parseInt(vehicles.id));
+// Get a single vehicle
+router.get('/:id', async (req, res) => {
+  const vehicle = await Vehicle.findById(req.params.id);
   if (!vehicle)
     res.status(404).send('The vehicle with the given id was not found!');
   res.send(vehicle);
 });
 
 // Update
-router.put('/:id', (req, res) => {
-  const vehicle = vehicles.find((v) => v.id === parseInt(req.params.id));
-  if (!vehicle)
-    return res.status(404).send('Vehicle with the given id was not found ');
-
+router.put('/:id', async (req, res) => {
   // Validate
   const { error } = validateVehicle(req.body);
   // Vehicle validation
   if (error) return res.status(400).send(error.details[0].message);
-
-  vehicle.name = req.body.name;
-  vehicle.category = req.body.category;
-  vehicle.transmission = req.body.transmission;
-  vehicle.engine = req.body.engine;
-  vehicle.fuelType = req.body.fuelType;
-  vehicle.colorExterior = req.body.colorExterior;
-  vehicle.colorInterior = req.body.colorInterior;
-  vehicle.price = req.body.price;
-
+  const vehicle = await Vehicle.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    {
+      new: true,
+    }
+  );
+  if (!vehicle)
+    return res.status(404).send('Vehicle with the given id was not found ');
   res.send(vehicle);
 });
-
 // Validate helper
 function validateVehicle(vehicle) {
   const schema = {
     name: Joi.string().required(),
-    category: Joi.string(),
-    transmission: Joi.string().length(10),
-    engine: Joi.number(),
-    fuelType: Joi.string().length(5),
-    colorExterior: Joi.string().length(10),
-    colorInterior: Joi.string().length(10),
-    price: Joi.number(),
+    // category: Joi.string(),
+    // transmission: Joi.string().length(10),
+    // engine: Joi.number(),
+    // fuelType: Joi.string().length(5),
+    // colorExterior: Joi.string().length(10),
+    // colorInterior: Joi.string().length(10),
+    // price: Joi.number(),
   };
 
   return Joi.validate(vehicle, schema);
 }
 
 // DELETE
-router.delete('/:id', (req, res) => {
-  const vehicle = vehicles.find((v) => v.id === parseInt(req.params.id));
+router.delete('/:id', async (req, res) => {
+  const vehicle = await Vehicle.findByIdAndRemove(req.params.id);
+
   if (!vehicle)
     return res
       .status(404)
       .send('The vehicle with the given id could not be deleted');
-  const index = vehicles.indexOf(vehicle);
-  vehicles.splice(index, 1);
-  return res.send(vehicle);
+  res.send(vehicle);
 });
 
 module.exports = router;
